72195287a34897a289440bd394ed2a057fc487af
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2020-04-07 18:49:25 +0100

    Added addoperation.h and added code for literal.h

---
+++ addoperation.h
@@ -0,0 +1,18 @@
+#ifndef ADDOPERATION_H
+#define ADDOPERATION_H
+#include "expression.h"
+
+class AddOperation : public Expression {
+private:
+    Expression *_left, *_right;
+    std::list<int> _variables;
+public:
+    AddOperation(Expression *left, Expression *right) : _left(left), _right(right), Expression() {
+        _variables = left->getVariables();
+        std::list<int> right_vars = right->getVariables();
+        //for (std::list<int>::iterator it = )
+    }
+
+};
+
+#endif
