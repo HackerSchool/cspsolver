7cafc68b89e0b0c0a790abea0eae4f4c78b5136d
henriquebg99 <henriquebg_99@hotmail.com>
2020-05-29 16:51:04 +0100

    #11 incomplete for mult

--- src/multoperation.cpp
+++ src/multoperation.cpp
@@ -5,5 +5,13 @@ std::shared_ptr<Value> MultOperation::eval(AssignmentSet *set) {
 }
 
 void MultOperation::check (AssignmentSet *set) {
-    
+    _left->check(set);
+    _right->check(set);
+
+    switch (_left->getType()->id()) {
+        case Type::ID_INTEGER:
+            
+            break;
+        
+    }
 }
